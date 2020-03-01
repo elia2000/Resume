ef2b94c0c788499131521566093cb5ff3acb4028
elia r <erahimi117@gmail.com>
2020-03-01 23:26:19 +0330

    Updates

---
+++ precache-manifest.187dcaa6d0a5dda5817115b245cbe8f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea0d67070f3fdadb444e638d93bc5c6f",
+    "url": "/Resume/index.html"
+  },
+  {
+    "revision": "0ccc79a7e5ac73457dfd",
+    "url": "/Resume/static/css/main.660b2df1.chunk.css"
+  },
+  {
+    "revision": "6e8216430f006f1ecdfa",
+    "url": "/Resume/static/js/2.17fcde20.chunk.js"
+  },
+  {
+    "revision": "4b1504a9f1a1ce101886e228862df6f3",
+    "url": "/Resume/static/js/2.17fcde20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ccc79a7e5ac73457dfd",
+    "url": "/Resume/static/js/main.23150102.chunk.js"
+  },
+  {
+    "revision": "a8bd4d37f56736925879",
+    "url": "/Resume/static/js/runtime-main.04142021.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
